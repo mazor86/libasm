eeab2b5414c7bf75d5ab0a66ef923de6d0c9f70e
mazor <mazor@student.21-school.ru>
2021-01-01 18:54:39 +0300

    add ft_strcmp

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -2,11 +2,17 @@
 
 				section		.text
 _ft_strcmp:
-				mov		rax, [rdi]
-				sub		rax, [rsi]
-				jnz		.return
-				cmp		byte [rdi], 0
-				
+				xor		rax, rax
+				xor		rdx, rdx
+				xor		rcx, rcx
+.loop:			mov		al, [rdi + rcx]	; rax = s1[i] 
+				mov		dl, [rsi + rcx]	; rcx = s2[i]
+				sub		rax, rdx	; rax = s1[i] - *s2[i]
+				jnz		.return		; if rax !=0 return (rax)
+				cmp		dl, 0
+				je		.return
+				inc		rcx
+				jmp		.loop
 
 .return:
 				ret
--- main.c
+++ main.c
@@ -6,12 +6,13 @@
 /*   By: mazor <mazor@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/28 15:03:57 by mazor             #+#    #+#             */
-/*   Updated: 2020/12/31 20:18:59 by mazor            ###   ########.fr       */
+/*   Updated: 2021/01/01 18:45:06 by mazor            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
+#include "libasm.h"
 
 int		main(void)
 {
@@ -28,9 +29,36 @@ int		main(void)
 
 	printf("_____ft_strcpy____\n\n");
 	char	dst[1024];
+	bzero(dst, 1024);
 	char	*src = "hello world";
 	ft_strcpy(dst, src);
 	printf("%s\n", dst);
-
+	
+	printf("\n\n");
+	printf("_____ft_strcmp____\n\n");
+	char	*s1 = "hello";
+	char	*s2 = "hello";
+	char	*s3 = "hello world";
+	char	*s4 = "";
+	char	*s5 = "";
+	printf("compare s1=\"%s\" s2=\"%s\"\n", s1, s2);
+	printf("strcmp = %d\n", strcmp(s1, s2));
+	printf("ft_strcmp = %d\n", ft_strcmp(s1, s2));
+	
+	printf("compare s1=\"%s\" s2=\"%s\"\n", s1, s3);
+	printf("strcmp = %d\n", strcmp(s1, s3));
+	printf("ft_strcmp = %d\n", ft_strcmp(s1, s3));
+	
+	printf("compare s1=\"%s\" s2=\"%s\"\n", s3, s2);
+	printf("strcmp = %d\n", strcmp(s3, s2));
+	printf("ft_strcmp = %d\n", ft_strcmp(s3, s2));
+	
+	printf("compare s1=\"%s\" s2=\"%s\"\n", s4, s5);
+	printf("strcmp = %d\n", strcmp(s4, s5));
+	printf("ft_strcmp = %d\n", ft_strcmp(s4, s5));
+		
+	printf("compare s1=\"%s\" s2=\"%s\"\n", s4, s2);
+	printf("strcmp = %d\n", strcmp(s4, s2));
+	printf("ft_strcmp = %d\n", ft_strcmp(s4, s2));
 	return (0);
 }
